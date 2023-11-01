c22d83fe0f1fd57401a7e845da41dba75f0d465b
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-11-01 15:51:33 -0400

    adds pricing, testimonials, contact, and blog

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./components/sidebar/sidebar";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Services from "./components/services/Services";
+import Resume from "./components/resume/Resume";
+import Portfolio from "./components/portfolio/Portfolio";
 
 const App = () => {
   return <div>App</div>;
