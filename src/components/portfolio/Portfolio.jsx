d898dbdb7faed18c4c499f45fc5ae6988723365a
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-11-06 18:27:31 -0500

    adds portfolio element

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,25 @@ import React from "react";
 import "./portfolio.css";
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  const project = {
+    title: "Top5",
+    imageSrc: "src/assets/tr3trqtert.jpg",
+    deployedLink: "https://top5-123-dd46eff5dfcf.herokuapp.com/",
+    repositoryLink: "https://github.com/rossgilman402/Top5",
+  };
+
+  return (
+    <div className="portfolio">
+      <div className="project">
+        <img src={project.imageSrc} alt={project.title} />
+        <h3>{project.title}</h3>
+        <div>
+          <a href={project.deployedLink}>Deployed Project</a>
+          <a href={project.repositoryLink}>Repository</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
