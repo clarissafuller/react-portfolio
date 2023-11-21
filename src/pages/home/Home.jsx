a181959d910c7ed503f18875318dce1e5a3f9426
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-11-21 15:28:24 -0500

    adds new elements to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,7 +7,17 @@ const Home = () => {
     <>
       <div className="main">
         <div className="title-container">
-          <h1>Anything is possible</h1>
+          <h1>Hi I'm</h1>
+          <h2>CLARISSA</h2>
+          <h1>i build things for the web</h1>
+        </div>
+        <div className="button-box">
+          <button
+            href="https://clarissafuller.com/about"
+            className="contact-button"
+          >
+            Contact Me
+          </button>
         </div>
       </div>
     </>
