5e7caa29c905d954172c400d79c470f0af3db415
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-11-30 13:53:53 -0500

    adds social links to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,11 @@
+import React from "react";
 import "./home.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faMedium,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Home = () => {
   return (
@@ -8,17 +15,39 @@ const Home = () => {
           <h1>Hi I'm</h1>
           <h2>CLARISSA</h2>
           <h1>i build things for the web</h1>
-          <p>
-            <a href="https://giphy.com/gifs/TGLt7x4Ppau1oEj21v"></a>
-          </p>
+        </div>
+        <div className="social-links">
+          {/* GitHub */}
+          <a
+            href="https://github.com/clarissafuller"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon className="icon" icon={faGithub} />
+          </a>
+
+          {/* Medium */}
+          <a
+            href="https://medium.com/@clarissafuller5"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon className="icon" icon={faMedium} />
+          </a>
+
+          {/* LinkedIn */}
+          <a
+            href="https://www.linkedin.com/in/clarissa-fuller-360096109/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon className="icon" icon={faLinkedin} />
+          </a>
         </div>
         <div className="button-box">
-          <button
-            href="https://clarissafuller.com/about"
-            className="contact-button"
-          >
+          <a href="https://clarissafuller.com/about" className="contact-button">
             Contact Me
-          </button>
+          </a>
         </div>
       </div>
     </>
