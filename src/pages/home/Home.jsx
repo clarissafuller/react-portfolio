29f2c4207a99424fda0d4111fe46b35c3f99f07c
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-11-30 15:03:23 -0500

    adds styling to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -13,41 +13,44 @@ const Home = () => {
       <div className="main-home">
         <div className="title-container">
           <h1>Hi I'm</h1>
-          <h2>CLARISSA</h2>
-          <h1>i build things for the web</h1>
-        </div>
-        <div className="social-links">
-          {/* GitHub */}
-          <a
-            href="https://github.com/clarissafuller"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon className="icon" icon={faGithub} />
-          </a>
+          <h1>CLARISSA</h1>
+          <h3>i build things for the web</h3>
+          <div className="social-links">
+            {/* GitHub */}
+            <a
+              href="https://github.com/clarissafuller"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon className="icon" icon={faGithub} />
+            </a>
 
-          {/* Medium */}
-          <a
-            href="https://medium.com/@clarissafuller5"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon className="icon" icon={faMedium} />
-          </a>
+            {/* Medium */}
+            <a
+              href="https://medium.com/@clarissafuller5"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon className="icon" icon={faMedium} />
+            </a>
 
-          {/* LinkedIn */}
-          <a
-            href="https://www.linkedin.com/in/clarissa-fuller-360096109/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon className="icon" icon={faLinkedin} />
-          </a>
-        </div>
-        <div className="button-box">
-          <a href="https://clarissafuller.com/about" className="contact-button">
-            Contact Me
-          </a>
+            {/* LinkedIn */}
+            <a
+              href="https://www.linkedin.com/in/clarissa-fuller-360096109/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon className="icon" icon={faLinkedin} />
+            </a>
+          </div>
+          <div className="button-box">
+            <a
+              href="https://clarissafuller.com/about"
+              className="contact-button"
+            >
+              Contact Me
+            </a>
+          </div>
         </div>
       </div>
     </>
